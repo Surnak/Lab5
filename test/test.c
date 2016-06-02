3e7480178512f2d7f7b113dced4ed0f5dbaf53e0
Surnak <ee521@mail.ru>
2016-06-03 02:21:07 +0600

    Created unit-test for the 1 root

--- test/test.c
+++ test/test.c
@@ -24,3 +24,26 @@ CTEST(equation_suite, two_roots_test)
     ASSERT_DBL_NEAR(expected_x1, x1);
     ASSERT_DBL_NEAR(expected_x2, x2);
 }
+
+CTEST(equation_suite, one_root_test)
+{
+    // Given
+    const double a = 2;
+    const double b = -4;
+    const double c = 2;
+
+    // When
+    double x1;
+    double x2;
+    int count;
+    roots(a, b, c, &x1, &x2, &count);
+
+    // Then
+    const double expected_count = 1;
+    const double expected_x1 = 1;
+    const double expected_x2 = 1;
+
+    ASSERT_DBL_NEAR(expected_count, count);
+    ASSERT_DBL_NEAR(expected_x1, x1);
+    ASSERT_DBL_NEAR(expected_x2, x2);
+}

817a6db5e81f8bb09c26d85ca15d9961c8ff82b7
Surnak <ee521@mail.ru>
2016-06-03 02:07:18 +0600

    Filled the main file

--- src/main.c
+++ src/main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include "roots.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+	if (argc < 4) {
+		fprintf(stderr, "Program: An error has occured!\nERROR: Missing coefficients!\nHint: Check your input value! It must be three coefficients!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	double a, b, c, x1, x2;
+	int count;
+
+	a = atof(argv[1]);
+	b = atof(argv[2]);
+	c = atof(argv[3]);
+
+	roots(a, b, c, &x1, &x2, &count);
+
+	if (count == 0) {
+		printf("No roots!\n");
+		exit(EXIT_SUCCESS);
+	}
+	else
+		if (count == 1) {
+			printf("Roots are equal!\nX = %f\n", x1);
+			exit(EXIT_SUCCESS);
+		}
+		else
+			if (count == 2) {
+				printf("X1 = %f\nX2 = %f\n", x1, x2);
+				exit(EXIT_SUCCESS);
+			}
 	return EXIT_SUCCESS;
 }
